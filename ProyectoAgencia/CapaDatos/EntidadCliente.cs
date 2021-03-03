2b895c31062c7e7fcac1dff0c4722b186f070bb6
Gabo1696 <gasa.96@hotmail.com>
2021-03-03 15:57:49 -0600

    Gabo y Johen

---
+++ ProyectoAgencia/CapaDatos/EntidadCliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class EntidadCliente
+    {
+        public int id_Cliente { get; set; }
+        public int cedula_cliente { get; set; }
+        public string nombre_Cliente { get; set; }
+        public string correo_Cliente { get; set; }
+        public string telefono_Cliente { get; set; }
+        public string direccion_cliente { get; set; }
+        public string contrasena { get; set; }
+    }
+}
