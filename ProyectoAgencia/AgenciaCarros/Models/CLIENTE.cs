d8d376382bfcac128ed4cc0432c2be6c57032f8a
gsosto <82732028+gsosto@users.noreply.github.com>
2021-04-27 02:04:50 -0600

    Added mensajes de error en cliente

--- ProyectoAgencia/AgenciaCarros/Models/CLIENTE.cs
+++ ProyectoAgencia/AgenciaCarros/Models/CLIENTE.cs
@@ -11,7 +11,8 @@ namespace AgenciaCarros.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class CLIENTE
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,13 +20,26 @@ namespace AgenciaCarros.Models
         {
             this.FACTURA = new HashSet<FACTURA>();
         }
-    
+
+        [Required(ErrorMessage = "El ID es obligatorio")]
         public int ID_CLIENTE { get; set; }
+        [Required(ErrorMessage = "La cedula es obligatoria")]
+        [MaxLength(9)]
+        [MinLength(9)]
         public int CEDULA { get; set; }
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+        [MaxLength(30)]
         public string NOMBRE { get; set; }
+        [Required(ErrorMessage = "El primer apellido es obligatorio")]
+        [MaxLength(30)]
         public string APELLIDO1 { get; set; }
+        [Required(ErrorMessage = "El segundo apellido es obligatorio")]
+        [MaxLength(30)]
         public string APELLIDO2 { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [MaxLength(30)]
         public string EMAIL { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio")]
         public int TELEFONO { get; set; }
         public Nullable<int> ID_DIRECCION { get; set; }
     
