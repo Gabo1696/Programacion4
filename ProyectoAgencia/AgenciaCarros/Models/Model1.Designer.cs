a91e20a860d3035c0e170312800f61855d4ffb2f
gsosto <82732028+gsosto@users.noreply.github.com>
2021-04-27 01:38:14 -0600

    Cambios al layout y vistas

--- ProyectoAgencia/AgenciaCarros/Models/Model1.Designer.cs
+++ ProyectoAgencia/AgenciaCarros/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Congo\Source\Repos\Programacion44\ProyectoAgencia\AgenciaCarros\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Congo\Source\Repos\Programacion4\ProyectoAgencia\AgenciaCarros\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
